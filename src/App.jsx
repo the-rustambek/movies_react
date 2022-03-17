70a45e532eae638eb25c9139e6e9e6fafed04d54
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-03-17 23:37:36 +0300

    add form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ 
+import './App.css';
+import FormChoose from './Component/FormChoose';
+
+function App() {
+  return (
+    <div className="App">
+      <FormChoose />
+    </div>
+  );
+}
+
+export default App;
